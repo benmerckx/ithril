d5d5bf72c94c27fba24fee1b9bbc02b8b5bcdeeb
Ben Merckx <ben@codeurs.be>
2016-02-07 14:40:06 +0100

    Submit script updated

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm ithril.zip 2> /dev/null
-zip -r ithril.zip src tests haxelib.json LICENSE.txt readme.md -x "*/\.*"
+zip -r ithril.zip src tests haxelib.json LICENSE.txt README.md -x "*/\.*"
 haxelib submit ithril.zip

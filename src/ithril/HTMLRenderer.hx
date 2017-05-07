70d73be6f478feea7f790803908302e219949b04
Russ Panula <dfadev@dfa.net>
2017-05-07 12:57:32 -0500

    - add async component lifecycle support to HTMLRenderer

--- src/ithril/HTMLRenderer.hx
+++ src/ithril/HTMLRenderer.hx
@@ -29,9 +29,16 @@ class HTMLRenderer {
         return outStr;
 	}
 
-	static function setHooks (component:Dynamic, vnode:Vnode, hooks:Array<Dynamic>) {
-		if (component.oninit != null) component.oninit(vnode);
-		if (component.onremove != null) hooks.push(function () component.onremove(vnode));
+	static function setHooks(component:Dynamic, vnode:Vnode, hooks:Array<Dynamic>) {
+		var promise = null;
+		if (component != null) {
+			if (component.oninit != null) promise = component.oninit(vnode);
+			if (component.onremove != null) hooks.push(function () return component.onremove(vnode));
+		}
+#if js
+		if (promise == null) return new js.Promise(function(resolve:Dynamic->Void, reject) resolve(null));
+#end
+		return promise;
 	}
 
 	static function attribute(name: String, value: Dynamic, escapeAttributeValue, namespace: Namespace) {
@@ -66,93 +73,217 @@ class HTMLRenderer {
 		return ' ' + (name == 'className' ? 'class' : name) + '="' + escapeAttributeValue(Std.string(value), true) + '"';
 	}
 
-	static function createAttrString(view: Dynamic, escapeAttributeValue, namespace: Namespace)
+	static function createAttrString(view:Dynamic, escapeAttributeValue, namespace: Namespace)
 		if (view.attrs == null)
 			return '';
 		else
 			return Reflect.fields(view.attrs).map(function(name) return attribute(name, Reflect.field(view.attrs, name), escapeAttributeValue, namespace)).join('');
 
-	static function createChildrenContent (view:Dynamic, options:Dynamic, hooks:Array<Dynamic>, level, namespace) {
-		var txt = (view.text == null || view.text == '') ? '' : spacer(options, level) + options.escapeString(view.text, true) + lineEnd(options);
+	static function createChildrenContent(view:Dynamic, options:Dynamic, hooks:Array<Dynamic>, level, namespace) {
+#if js
+		return new js.Promise(function (resolve, reject) {
+#end
+			var txt = (view.text == null || view.text == '') ? '' : spacer(options, level) + options.escapeString(view.text, true) + lineEnd(options);
 
-		if (Std.is(view.children, Array) && view.children.length == 0)
-			return txt;
-		else
-			return txt + _render(view.children, options, hooks, level, namespace);
+#if !js
+			if (Std.is(view.children, Array) && view.children.length == 0)
+				return txt;
+			else
+				return txt + Std.string(_render(view.children, options, hooks, level, namespace));
+#else
+			if (Std.is(view.children, Array) && view.children.length == 0)
+				resolve(txt);
+			else
+				_render(view.children, options, hooks, level, namespace).then(function(rslt) resolve(txt + rslt));
+		});
+#end
 	}
 
-	public static function render (view:Dynamic, ?attrs:Dynamic, ?options:Dynamic) {
-		if (Std.is(view, Class)) return render({ tag: view, attrs: attrs }, attrs, options);
-		var hooks = [];
+	static public function render (view:Dynamic, ?attrs:Dynamic, ?options:Dynamic)
+#if js :js.Promise<Dynamic> #end
+	{
+#if js
+		return new js.Promise(function (resolve, reject) {
+			if (Std.is(view, Class)) {
+				render({ tag: view, attrs: attrs }, attrs, options).then(function (rslt) resolve(rslt));
+				return;
+			}
+#else
+			if (Std.is(view, Class)) return render({ tag: view, attrs: attrs }, attrs, options);
+#end
+			var hooks = [];
 
-		var defaultOptions = {
-			escapeAttributeValue: escapeHtml,
-			escapeString: escapeHtml,
-			strict: false,
-			space: ''
-		}
+			var defaultOptions = {
+				escapeAttributeValue: escapeHtml,
+				escapeString: escapeHtml,
+				strict: false,
+				space: ''
+			}
 
-		if (options == null)
-			options = defaultOptions;
-		else
-			Reflect.fields(defaultOptions).map(function (key)
-				if (!Reflect.hasField(options, key)) Reflect.setField(options, key, Reflect.field(defaultOptions, key)));
+			if (options == null)
+				options = defaultOptions;
+			else
+				Reflect.fields(defaultOptions).map(function (key) if (!Reflect.hasField(options, key)) Reflect.setField(options, key, Reflect.field(defaultOptions, key)));
 
-		var result = _render(view, options, hooks);
-		hooks.map(function (hook) { hook(); });
+#if !js
+			var result = _render(view, options, hooks);
+			hooks.map(function (hook) { hook(); });
 
-		return result;
-	}
-
-	static function _render (view:Dynamic, options:Dynamic, hooks:Array<Dynamic>, level = 0, namespace = Namespace.None) {
-		if (Std.is(view, String)) return spacer(options, level) + options.escapeString(view, false) + lineEnd(options);
-
-		if (Std.is(view, Int) || Std.is(view, Float) || Std.is(view, Bool)) return view;
-
-		if (view == null) return '';
-
-		if (Std.is(view, Array)) {
-			var result = '';
-			for (v in (view:Array<Dynamic>))
-				result += _render(v, options, hooks, level + 1, namespace);
 			return result;
-		}
+#else
+			_render(view, options, hooks)
+				.then(function (rslt) {
+					js.Promise.all([for (h in hooks) h()]) // { var prom = h(); if (prom != null) (prom:js.Promise); }])
+					.then(function (r) resolve(rslt));
+				});
+				//hooks.map(function (hook) { hook(); });
+				//resolve(result);
+			//});
+		});
+#end
+	}
 
-		if (view.attrs) setHooks(view.attrs, view, hooks);
+	static function _render (view:Dynamic, options:Dynamic, hooks:Array<Dynamic>, level = 0, namespace = Namespace.None)
+#if js :js.Promise<Dynamic> #end
+	{
+#if js
+		return new js.Promise(function (resolve, reject) {
+#end
+			if (Std.is(view, String))
+#if !js
+				return spacer(options, level) + options.escapeString(view, false) + lineEnd(options);
+#else
+			{
+				resolve(spacer(options, level) + options.escapeString(view, false) + lineEnd(options));
+				return;
+			}
+#end
 
-		var vnode:Vnode = {
-			tag: view.tag,
-			children: view.children,
-			attrs: view.attrs != null ? view.attrs : {}
-		}
+			if (Std.is(view, Int) || Std.is(view, Float) || Std.is(view, Bool))
+#if !js
+				return spacer(options, level) + view + lineEnd(options);
+#else
+			{
+				resolve(spacer(options, level) + view + lineEnd(options));
+				return;
+			}
+#end
 
-		if (Std.is(view.tag, Class)) {
-			var component = Type.createInstance(view.tag, [vnode]);
-			vnode.state = component;
-			setHooks(component, vnode, hooks);
-			return _render(component.view(vnode), options, hooks, level, namespace);
-		}
+			if (view == null)
+#if !js
+				return '';
+#else
+			{
+				resolve('');
+				return;
+			}
+#end
 
-		if (view.tag == '<') return spacer(options, level) + view.children + lineEnd(options);
+			if (Std.is(view, Array)) {
+#if !js
+				var result = '';
+				for (v in (view:Array<Dynamic>))
+					result += _render(v, options, hooks, level + 1, namespace);
+				return result;
+#else
+				js.Promise.all([for (v in (view:Array<Dynamic>)) _render(v, options, hooks, level + 1, namespace)])
+					.then(function (result) resolve('' + result.join('')));
+				return;
+#end
+			}
 
-		if (view.tag == 'svg') {
-			namespace = Namespace.Svg;
-			view.attrs.xmlns = namespace;
-		}
+#if !js
+			if (view.attrs) setHooks(view.attrs, view, hooks);
+#else
+			setHooks(view.attrs, view, hooks).then(function (z) {
+#end
+				var vnode:Vnode = {
+					tag: view.tag,
+					children: view.children,
+					attrs: view.attrs != null ? view.attrs : {}
+				};
 
-		var children = createChildrenContent(view, options, hooks, level, namespace);
-		if (view.tag == '#') return spacer(options, level) + options.escapeString(children, false) + lineEnd(options);
+				if (Std.is(view.tag, Class)) {
+					var component = Type.createInstance(view.tag, [vnode]);
+					vnode.state = component;
+#if !js
+					setHooks(component, vnode, hooks);
+					return _render(component.view(vnode), options, hooks, level, namespace);
+#else
+					setHooks(component, vnode, hooks)
+						.then(function (z) _render(component.view(vnode), options, hooks, level, namespace)
+							.then(function (rslt) resolve(rslt)));
+					return;
+#end
+				}
 
-		if (view.tag == '[') return children;
+				if (view.tag == '<')
+#if !js
+					return spacer(options, level) + view.children + lineEnd(options);
+#else
+				{
+					resolve(spacer(options, level) + view.children + lineEnd(options));
+					return;
+				}
+#end
 
-		if (children != null && (options.strict || (Std.is(view.tag, String) && VOID_TAGS.indexOf(view.tag.toLowerCase()) >= 0)))
-			return spacer(options, level) + '<' + view.tag + createAttrString(view, options.escapeAttributeValue, namespace) + (options.strict ? '/' : '') + '>' + lineEnd(options);
+				if (view.tag == 'svg') {
+					namespace = Namespace.Svg;
+					view.attrs.xmlns = namespace;
+				}
 
-		return [
-			spacer(options, level) + '<', view.tag, createAttrString(view, options.escapeAttributeValue, namespace), '>' + lineEnd(options),
-			children,
-			spacer(options, level) + '</', view.tag, '>' + lineEnd(options)
-		].join('');
+#if !js
+				var children = createChildrenContent(view, options, hooks, level, namespace);
+#else
+				createChildrenContent(view, options, hooks, level, namespace).then(function (children) {
+#end
+					if (view.tag == '#')
+#if !js
+						return spacer(options, level) + options.escapeString(children, false) + lineEnd(options);
+#else
+					{
+						resolve(spacer(options, level) + options.escapeString(children, false) + lineEnd(options));
+						return;
+					}
+#end
+
+					if (view.tag == '[')
+#if !js
+						return children;
+#else
+					{
+						resolve(children);
+						return;
+					}
+#end
+
+					if (children != null && (options.strict || (Std.is(view.tag, String) && VOID_TAGS.indexOf(view.tag.toLowerCase()) >= 0)))
+#if !js
+						return spacer(options, level) + '<' + view.tag + createAttrString(view, options.escapeAttributeValue, namespace) + (options.strict ? '/' : '') + '>' + lineEnd(options);
+#else
+					{
+						resolve(spacer(options, level) + '<' + view.tag + createAttrString(view, options.escapeAttributeValue, namespace) + (options.strict ? '/' : '') + '>' + lineEnd(options));
+						return;
+					}
+#end
+
+					var rslt:String = [
+						spacer(options, level) + '<', view.tag, createAttrString(view, options.escapeAttributeValue, namespace), '>' + lineEnd(options),
+						children,
+						spacer(options, level) + '</', view.tag, '>' + lineEnd(options)
+					].join('');
+#if !js
+					return rslt;
+#else
+					resolve(rslt);
+					return;
+#end
+
+#if js
+				});
+			});
+		});
+#end
 	}
 
 	static public function lineEnd(options) return options.space == '' ? '' : "\n";

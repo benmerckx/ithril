a26aeb979b7cc199779291382ca272baf00e4d4e
Russ Panula <dfadev@dfa.net>
2017-05-13 15:30:53 -0500

    - add examples directory to zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-zip -r ithril.zip src tests haxelib.json LICENSE.txt README.md -x "*/\.*"
+zip -r ithril.zip src tests examples haxelib.json LICENSE.txt README.md -x "*/\.*"
 haxelib submit ithril.zip
 rm ithril.zip 2> /dev/null
